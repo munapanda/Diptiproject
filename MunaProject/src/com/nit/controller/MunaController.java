995e52538573bbcbce18ad7a6d32a467d45a4416
dev1 <dev@gmail.com>
2020-09-29 09:56:14 +0530

    commited

--- MunaProject/src/com/nit/controller/MunaController.java
+++ MunaProject/src/com/nit/controller/MunaController.java
@@ -6,6 +6,7 @@ public class MunaController {
 		System.out.println("Kuna is a gandu boy");
 		System.out.println("Muna is a good boy");
 		System.out.println("KunA IS SUPER GANDU");
+		System.out.println("Kuna is dumb fellow");
 	}
 	}
 
