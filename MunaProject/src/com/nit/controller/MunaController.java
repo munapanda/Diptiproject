917f9fdeadba5d8b3943569a0ec879538d3f85c9
dev1 <dev@gmail.com>
2020-09-29 09:42:26 +0530

    kUNA COMMIEDE

--- MunaProject/src/com/nit/controller/MunaController.java
+++ MunaProject/src/com/nit/controller/MunaController.java
@@ -5,6 +5,7 @@ public class MunaController {
 	public static void main(String[] args) {
 		System.out.println("Kuna is a gandu boy");
 		System.out.println("Muna is a good boy");
+		System.out.println("KunA IS SUPER GANDU");
 	}
 	}
 
